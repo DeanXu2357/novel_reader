b126ff7846bb62e3051ac4084910c8baa4e73919
poyu Xu <jasugun0000@gmail.com>
2018-09-12 10:35:07 +0800

    book model改為讀取設定檔

--- model/book/book.go
+++ model/book/book.go
@@ -2,9 +2,11 @@ package book
 
 import (
     "fmt"
+    "strings"
 
     "github.com/jinzhu/gorm"
     _ "github.com/jinzhu/gorm/dialects/postgres"
+    "github.com/spf13/viper"
 )
 
 var db *gorm.DB
@@ -19,7 +21,26 @@ type Book struct {
 }
 
 func init() {
-    db, err = gorm.Open("postgres", "host= port= user= dbname= password=")
+    viper.SetConfigType("yaml")
+    viper.SetConfigName("config")
+    viper.AddConfigPath(".")
+    viper.SetEnvPrefix("dev")
+    viper.AutomaticEnv()
+    viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+    if err:= viper.ReadInConfig(); err != nil {
+        fmt.Println(err)
+    }
+}
+
+func init() {
+    db_info := "host=" + viper.GetString("DB_HOST") 
+    db_info += " port=" + viper.GetString("DB_PORT") 
+    db_info += " user=" + viper.GetString("DB_USER") 
+    db_info += " dbname=" + viper.GetString("DB_NAME") 
+    db_info += " password=" + viper.GetString("DB_PASS")
+
+    db, err = gorm.Open("postgres", db_info)
 
     if err != nil {
         fmt.Println(err)
