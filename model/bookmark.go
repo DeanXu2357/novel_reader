7791157c05afb0653f406aff241d5604564b70f0
Dean Xu <dean.xu.2357@gmail.com>
2018-09-17 16:49:19 +0800

    書籤添加複合索引(user+book)&整理程式碼

--- model/bookmark.go
+++ model/bookmark.go
@@ -7,17 +7,16 @@ import (
 	Db "reader_api/database"
 
 	"github.com/jinzhu/gorm"
-	// "reader_api/model/book"
 )
 
 // Bookmark 書籤基本類
 type Bookmark struct {
 	gorm.Model
 	// Book books.Book `gorm:"foreignkey:Book_id"`
-	UserID uint
-	BookID uint
-	Chap   uint `gorm:"default:0"`
-	Line   uint `gorm:"default:0"`
+    UserID uint `gorm:"unique_index:user_book_uk"`
+    BookID uint `gorm:"unique_index:user_book_uk"`
+    Chap   uint `gorm:"default:0"`
+    Line   uint `gorm:"default:0"`
 }
 
 func init() {
@@ -25,18 +24,13 @@ func init() {
 	// db.Model(&BookMark).Related(&book.Book)
 }
 
-// var bookmarks []Bookmark
-// var err error
-
-// 新增
 // 修改
 // 刪除
 
-// List 顯示全部
+// List 顯示該 userID 書籤列表
 func (bookmark Bookmark) List(userID string) (bookmarks []Bookmark, err error) {
 	if err = Db.Orm.Where("user_id = ?", userID).Find(&bookmarks).Error; err != nil {
 		fmt.Println("[Model Error]")
-		// fmt.Printf("%+v\n", err)
 		fmt.Println(err)
 		return
 	}
@@ -61,30 +55,7 @@ func (bookmark *Bookmark) Create() (err error) {
     return
 }
 
-// 取得該User的所有書籤
-// func ListByUser(user_id string) *gorm.DB { //  ([]Bookmark, []error) {
-// var bookmarks []Bookmark
+// func (bookmark *Bookmark) Find(userID string, bookID string) (err error) {
 
-// err := db.Where("user_id = ?", user_id).Find(&bookmarks).GetErrors
-
-// return bookmarks, err
-
-//     db.DB().Ping()
-
-//     return db.Where("user_id = ?", user_id)
 // }
 
-// 取得指定的書籤（用User&Book查找
-func FindByUserAndBook() {
-	// code...
-}
-
-// 刪除指定書籤
-func Delete() {
-	// code...
-}
-
-// 編輯指定書籤的 chap & line
-func Edit() {
-	// code...
-}
