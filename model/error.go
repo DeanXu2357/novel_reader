d0fbaea8add1d797b263d967fa7b6299cb68dbc4
Dean Xu <dean.xu.2357@gmail.com>
2018-09-23 20:53:47 +0800

    添加新的錯誤訊息

--- model/error.go
+++ model/error.go
@@ -1,10 +1,14 @@
 package model
 
 import (
+	"errors"
+
 	"github.com/jinzhu/gorm"
 )
 
 var (
 	// ErrRecordNotFound gorm 的空值錯誤
 	ErrRecordNotFound = gorm.ErrRecordNotFound
+	// ErrDataExisted 資料已存在
+	ErrDataExisted = errors.New("Data existed")
 )
